774946f56f9704468473441679405948b419dc52
Nadeem Abdul Hamid <nadeem@acm.org>
2017-10-10 13:24:28 -0400

    initial commit

---
+++ cacher.rkt
@@ -0,0 +1,333 @@
+#lang racket
+
+(require file/md5
+         json
+         "util.rkt")
+
+(module+ test
+  (require rackunit))
+
+(provide NEVER-CACHE
+         NEVER-RELOAD
+         sinbad-cache-enabled
+         default-cacher
+         clear-cache
+         update-timeout
+         update-directory)
+
+;; *parameter* to globally enable/disable caching behavior
+(define sinbad-cache-enabled (make-parameter #t (lambda (v) (equal? v #t))))
+
+;; constants - public
+(define NEVER-CACHE 0)
+(define NEVER-RELOAD -1)
+
+;; constants - private
+(define DEFAULT-CACHE-DIR     (build-path (find-system-path 'temp-dir) "sinbad_cache"))
+(define MINIMUM-TIMEOUT-VALUE 100)   ; milliseconds
+
+
+
+(struct cacher (directory expiration))
+
+(define default-cacher (cacher DEFAULT-CACHE-DIR MINIMUM-TIMEOUT-VALUE))
+
+;; cacher string -> cacher
+;; Returns an new cacher like c but with the given cache directory
+(define (update-directory c new-dir)
+  (struct-copy cacher c [directory new-dir]))
+
+;; cacher number -> cacher
+;; Returns an new cacher like c but with the given cache expiration value
+;; specified in milliseconds.
+(define (update-timeout c new-to)
+  (define checked-to
+    (if (< 0 new-to MINIMUM-TIMEOUT-VALUE)
+        (begin
+          (fprintf (current-error-port)
+                   "Warning: cannot set cache timeout to less than ~a msec.~n" MINIMUM-TIMEOUT-VALUE)
+          MINIMUM-TIMEOUT-VALUE)
+        new-to))
+  (struct-copy cacher c [expiration checked-to]))
+
+(module+ test
+  (let ([sp (open-output-string)])
+    (parameterize ([current-error-port sp])  ; (open-output-nowhere)
+      (check-equal? (cacher-expiration (update-timeout default-cacher 10))
+                    MINIMUM-TIMEOUT-VALUE)
+      (check-true   (string-prefix? (get-output-string sp) "Warning:"))
+      (check-equal? (cacher-expiration (update-timeout default-cacher NEVER-CACHE))
+                    NEVER-CACHE)
+      (check-equal? (cacher-expiration (update-timeout default-cacher 10000))
+                    10000))))
+
+
+;; cacher string string -> boolean
+;; Determine if the given path+subtag is not in the cache or expired.
+(define (stale? c path subtag)
+  (cond
+    [(not (and (sinbad-cache-enabled) (cacheable? c path subtag)))  #t]
+    [else
+     (define e (cache-entry-for c path subtag))
+     (or (false? e)
+         (expired? e (cacher-expiration c)))]))
+
+
+;; cacher string string -> boolean
+;; Delete the stored data in the cache directory and remove info from the cache index file
+(define (clear-cache-data c tag subtag)
+  (define e (cache-entry-for c tag subtag))
+  (if (not e)
+      #f
+      (begin
+        (when (entry-data-valid? e)
+          (delete-file (entry-data e)))
+        (remove-entry c e))))
+
+
+;; cacher string string -> #f or entry
+;; Return the cache entry object for the tag+subtag pair if it exists
+(define (cache-entry-for c tag subtag)
+  (define ces (read-cache-entry-list (cache-index-file-path c tag)))
+  (findf
+   (lambda (e) (and (string=? tag (entry-tag e))
+                    (string=? subtag (entry-subtag e))))
+   ces))
+
+
+;; cacher string string -> #f or string
+(define (lookup-entry-data c tag subtag)
+  (define e (cache-entry-for c tag subtag))
+  (cond
+    [(false? e) #f]
+    [(expired? e (cacher-expiration c))
+     (clear-cache-data c tag subtag)
+     #f]
+    [else (entry-data e)]))
+
+
+;; cacher entry -> boolean
+;; Replace the cache entry in the cache index file that has the same 
+;; tag+subtag pair as the given cache entry with the given cache entry. 
+;; If there is not already an entry for that tag+subtag, then the given
+;; entry is added to the list in the index file.
+(define (add-or-update-entry c e-new)
+  (define cache-idx (cache-index-file-path c (entry-tag e-new)))
+  (define ces (cons e-new
+                    (remove e-new
+                            (read-cache-entry-list cache-idx)
+                            entry-tags-match)))
+  (write-cache-entry-list cache-idx ces))
+
+
+
+;; cacher entry -> boolean
+;; Remove a particular entry from the index file that it belongs to.
+(define (remove-entry c e-key)
+  (define cache-idx (cache-index-file-path c (entry-tag e-key)))
+  (define ces (filter
+               (lambda (e) (not (entry-tags-match e-key e)))
+               (read-cache-entry-list cache-idx)))
+  (write-cache-entry-list cache-idx ces))
+
+
+
+;; cacher string [input-port or #f] -> (list string [string or #f] [string or #f])
+
+
+
+
+
+
+
+
+
+
+
+
+
+;; cacher string [bytes or string] [#f or string] -> string
+;; Creates a temporary file in the cache directory and writes the 
+;; given data to it, encoding it first if the data is a string
+;;
+;; tag is used to determine the subdirectory in which the temp file is created.
+;; Note: a new temporary file is created each time this function is called
+;;
+;; Returns the path (string) to the newly-created file 
+(define (cache-byte-data c tag data [enc #f])
+  (define stuff
+    (cond [(bytes? data) data]
+          [(string? enc) (parameterize ([current-locale enc])
+                           (string->bytes/locale data))]
+          [else (string->bytes/utf-8 data)]))
+  
+  (define cache-dir (build-path (cacher-directory c)
+                                (cache-subdir-name tag)))
+  
+  (when (not (directory-exists? cache-dir))
+    (make-directory* cache-dir))
+
+  (define temp-path (make-temporary-file "cache~a.dat" #f cache-dir))
+  
+  (with-output-to-file temp-path
+    (lambda () (write-bytes stuff))
+     #:exists 'replace)
+  
+  (path->string temp-path))
+
+
+;; Delete the entire cache directory being maintained by the given cacher
+(define (clear-cache c)
+  (when (directory-exists? (cacher-directory c))
+    (delete-directory/files (cacher-directory c))))
+
+
+(define (cacheable? c path subtag)
+  ;; if it's something other than "main" data being accessed (e.g. "schema")
+  ;;   then it can be in the cache, even if the main is a local file
+  ;; otherwise, if it's the "main" data we're considering, then only cache
+  ;;  if it smells like a URL and the cacher specific setting is not set 
+  ;;  to never cache
+  (and (sinbad-cache-enabled)
+       (or (not (string-prefix? subtag "main"))
+           (and (smells-like-url? path)
+                (>= (cacher-expiration c) 0)))))
+
+(module+ test
+  (check-true (cacheable? default-cacher "http://cs.berry.edu" "main"))
+  (check-true (cacheable? default-cacher "cacher.rkt" "schema"))
+  
+  (check-false (cacheable? default-cacher "cacher.rkt" "main"))
+  )
+
+
+;; cacher  string  ->  path or false
+;; gets the cache index file file for the given tag under the given cacher
+;; the file must exist (is created and filled with empty list of entries if
+;; it doesn't already)
+(define (cache-index-file-path c tag)
+  (define D (cacher-directory c))
+  
+  (when (not (directory-exists? D))
+    (make-directory* D))
+  (cond
+    [(not (directory-exists? D))   #f]
+    [else
+     (define cache-idx (path->complete-path (build-path D (cache-index-name tag))))
+     (when (not (file-exists? cache-idx))
+       (write-cache-entry-list cache-idx empty))
+     (and (file-exists? cache-idx)
+          cache-idx)]))
+
+
+(define (cache-index-name tag)
+  (string-append  "idx" (bytes->string/utf-8 (md5 tag)) ".json"))
+
+(define (cache-subdir-name tag)
+  (string-append "dat" (bytes->string/utf-8 (md5 tag))))
+
+;; string/path -> (listof entry)
+(define (read-cache-entry-list cache-idx-name)
+  (with-input-from-file cache-idx-name
+    (lambda () (json->entries (read-json)))))
+
+;; string/path (listof entry) -> boolean
+(define (write-cache-entry-list cache-idx-name ces)
+  (with-output-to-file
+      cache-idx-name
+    (lambda () (write-json (entries->json ces)))
+    #:exists 'replace)
+  #t)
+
+
+
+(module+ test
+  (check-equal? (cache-index-name "http://cs.berry.edu")
+                "idx0f1cacfe4398213523f4943524a234de.json")
+  (check-equal? (cache-subdir-name "http://cs.berry.edu")
+                "dat0f1cacfe4398213523f4943524a234de"))
+
+
+
+
+;;;; ================ cache entry structure ===============================
+
+;; cachedata is a local file page
+;; timestamp is in milliseconds
+(struct entry (tag subtag data timestamp) #:transparent)
+
+
+
+;; jsexpr -> (listof entry)
+(define (json->entries jse)
+  (define (json->entry e)
+    (entry (hash-ref e 'tag)
+           (hash-ref e 'subtag)
+           (hash-ref e 'cachedata)
+           (hash-ref e 'timestamp)))
+  
+  (match jse
+    [(list e ...) (map json->entry e)]
+    [_ '()]))
+
+
+;; (listof entry) -> jsexpr
+(define (entries->json es)
+  (map (lambda (e)
+         (make-hasheq `((tag . ,(entry-tag e))
+                        (subtag . ,(entry-subtag e))
+                        (cachedata . ,(entry-data e))
+                        (timestamp . ,(entry-timestamp e)))))
+       es))
+
+(module+ test
+  (let ([es (list (entry "aaa" "bb" "blah" 10)
+                (entry "ccc" "dd" "bfoolah" 20))])
+    (check-equal? (json->entries (entries->json es))
+                  es)))
+
+
+
+;; entry -> boolean
+;; Check to see if the entry's cachedata refers to an actual readable file.
+(define (entry-data-valid? e)
+  (and (entry-data e)
+       (file-exists? (entry-data e))
+       (member 'read (file-or-directory-permissions (entry-data e)))
+       (member 'write (file-or-directory-permissions (entry-data e)))
+       #t))
+
+(module+ test
+  (check-true (entry-data-valid? (entry "aaa" "bbb" "cacher.rkt" 10)))
+  (check-false (entry-data-valid? (entry "aaa" "bbb" "frooblah" 10))))
+
+
+
+;; entry entry -> boolean
+;; determine if a pair of tag+subtag's match
+(define (entry-tags-match e1 e2)
+  (and (string=? (entry-tag e1) (entry-tag e2))
+       (string=? (entry-subtag e1) (entry-subtag e2))))
+
+(module+ test
+  (check-true (entry-tags-match (entry "aaa" "bbb" "blah" 10)
+                                (entry "aaa" "bbb" "bfoolah" 20)))
+  (check-false (entry-tags-match (entry "aaa" "bbb" "blah" 10)
+                                 (entry "aaa" "bbc" "blah" 10))))
+
+
+;; entry number[milliseconds] -> boolean
+;; Check to see if the given entry has expired (relative to current time).
+
+(define (expired? ent expiration)
+  (define diff (- (current-inexact-milliseconds)
+                  (entry-timestamp ent)))
+  (and (>= expiration 0)
+       (> diff expiration)))
+
+(module+ test
+  (check-true (expired? (entry "aaa" "bbb" "blah" (- (current-inexact-milliseconds) 10))
+                          5))
+  (check-false (expired? (entry "aaa" "bbb" "blah" (- (current-inexact-milliseconds) 10))
+                          15)))
+
---
+++ util.rkt
@@ -0,0 +1,103 @@
+#lang racket
+
+(require file/gunzip
+         net/url)
+
+(provide smells-like-url?
+         smells-like-gzip?
+         create-input
+         raw-create-input)
+
+(module+ test
+  (require rackunit))
+
+
+;; Determine if the given path seems like a URL
+(define (smells-like-url? p)
+  ;;     Currently, only things that start off http://
+  ;;     https:// or ftp:// are treated as URLs.
+  (and (string-contains? p "://")
+       (or (string-prefix? p "http")
+           (string-prefix? p "ftp"))))
+
+
+(define (smells-like-gzip? p)
+  (string-contains? p ".gz"))
+
+
+
+
+(define (create-input path)
+  (first (raw-create-input path)))
+
+
+#|
+    Returns a triple, ( fp, real_name, enc ), containing a file-type object
+    for the given path, an alternate name for the resource, and an encoding.
+    
+    If the path is a normal file, produces a file-object for that file.
+    
+    If the path is a URL, makes a request to that URL and
+    returns an input stream to read the response.
+    
+    In the process of processing the response from the URL,
+    if a name for the file is found (e.g. in 
+    a Content-Disposition header), that is produced as real_name. 
+    Otherwise real_name is returned as #f.
+    
+    If an encoding is determined from the URL response
+    headers, it is included, otherwise the third element 
+    of the triple is #f.
+|#
+(define (raw-create-input path)
+  (define charset (box #f))
+  (define local-name (box #f))
+
+  (define fp
+    (cond
+      [(or (not path)
+           (not (string? path))) #f]
+      [(smells-like-url? path)
+       (define-values (ip hdrs) (get-pure-port/headers (string->url path)
+                                                       (list "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36")))
+       (for ([h (string-split hdrs #px"[\r\n]")])
+         (define R1 (regexp-match #px"^[Cc]ontent-[Dd]isposition.*filename=\"?([^\\s\"]*)\"?" h))
+         (define R2 (regexp-match #px"^Content-Type:.*charset=\"?([^\\s\"]*)\"?" h))
+         (when R1 (set-box! local-name (second R1)))
+         (when R2 (set-box! charset (second R2))))
+       ip]
+    [(string-prefix? path "wss:")  #f]
+    [else (open-input-file path)]))
+
+  (define final-fp
+    (cond
+      [(or (smells-like-gzip? path)
+            (and (string? (unbox local-name))
+                 (smells-like-gzip? (unbox local-name))))
+
+       (define ubp (open-output-bytes))
+       (gunzip-through-ports fp ubp)
+       (define unzipped-bytes-port (open-input-bytes (get-output-bytes ubp)))
+       (close-input-port fp)   ; is this safe here?
+       unzipped-bytes-port]
+      [else fp]))
+  
+
+  (list final-fp (unbox local-name) (unbox charset)))
+
+
+
+#;
+(module+ test
+  ; content disposition header...
+  (check-equal? (second
+                 (raw-create-input "http://api.worldbank.org/v2/en/country/per?downloadformat=csv"))
+                 "API_PER_DS2_en_csv_v2.zip")
+  ; only one file in the zip...
+  (check-equal? (second
+                 (raw-create-input "http://api.worldbank.org/v2/en/indicator/SP.POP.TOTL?downloadformat=xml"))
+                 "API_SP.POP.TOTL_DS2_en_xml_v2.zip")
+  ; gzipped input...
+  (check-equal? (read (create-input "https://www1.ncdc.noaa.gov/pub/data/swdi/stormevents/csvfiles/StormEvents_details-ftp_v1.0_d1950_c20170120.csv.gz"))
+                'BEGIN_YEARMONTH))
+
