03a1be9f39bf964ed413bd2dbdacef8876c43784
Nadeem Abdul Hamid <nadeem@acm.org>
2017-10-26 20:48:19 -0400

    fix info

--- info.rkt
+++ info.rkt
@@ -1,22 +1,5 @@
-#lang info
+#lang setup/infotab
 
-
-(define name "sinbad")
-(define version "0.0.1")
-
-(define deps '("csv-reading"
-               "sxml"))
-
-(define primary-file "main.rkt")
-
-(define release-notes
-  (list '(ul
-          (li "Proof-of-concept implementation")
-          )))
-
-(define required-core-version "6.9")
-
-(define blurb (list "Automated Structure Inference and Binding of Data"))
-
-;(define scribblings '(("scribblings/racketui.scrbl" (multi-page))))
-
+(define collection 'multi)
+;(define deps '("base"))
+;(define build-deps '("racket-doc" "scribble-lib"))
